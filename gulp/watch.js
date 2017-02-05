0bddaba9a78f1051504492bc59dccd6cb69e960b
Sean Dietrich <sean.dietrich@inresonance.com>
2017-02-04 21:50:07 -0800

    Initial commit

---
+++ gulp/watch.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var browserSync = require('browser-sync');
+
+function isOnlyChange(event)
+{
+  return event.type === 'changed';
+}
+
+gulp.task('watch', ['inject'], function ()
+{
+  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
+
+  gulp.watch([
+    path.join(conf.paths.src, '/app/**/*.css'),
+    path.join(conf.paths.src, '/app/**/*.scss')
+  ], function (event)
+  {
+    if ( isOnlyChange(event) )
+    {
+      gulp.start('styles-reload');
+    }
+    else
+    {
+      gulp.start('inject-reload');
+    }
+  });
+
+  gulp.watch(path.join(conf.paths.src, '/app/**/*.js'), function (event)
+  {
+    if ( isOnlyChange(event) )
+    {
+      gulp.start('scripts-reload');
+    }
+    else
+    {
+      gulp.start('inject-reload');
+    }
+  });
+
+  gulp.watch([
+    path.join(conf.paths.src, '/index.html'),
+    path.join(conf.paths.src, '/app/**/*.{scss,css,js}'),
+    path.join(conf.paths.src, '/app/**/*.json'),
+    path.join(conf.paths.src, '/app/**/*.html')
+  ], function (event)
+  {
+    browserSync.reload(event.path);
+  });
+});
