dbf1a6f9cbab24d33fc56d4bd6e9f66d2b38fd2f
Sean Dietrich <sean.e.dietrich@gmail.com>
2017-02-18 11:32:54 -0800

    Create index.module.js

---
+++ src/app/index.module.js
@@ -0,0 +1,18 @@
+(function ()
+{
+  'use strict';
+
+  /**
+   * Main module of the Atlas
+   */
+  angular
+  .module('angularSkeleton', [
+    'ngAnimate',
+    'ngAria',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngMaterial',
+    'ui.router'
+  ]);
+})();
